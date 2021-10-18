55a1b0fa33f8a64a3a96eea020558284bd7cbd94
Prabhjot <prabhx.jott@gmail.com>
2021-10-18 23:09:43 +0530

    done with basics of ll

--- Linked List/Circular Doubly Linked List/display.cpp
+++ Linked List/Circular Doubly Linked List/display.cpp
@@ -1,9 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    Node *prev;
+    int data;
+    Node *next;
+} * head;
+
+void createCircularDoublyLL(int a[], int size)
+{
+    Node *p;
+    Node *temp;
+    head = new Node;
+    head->prev = NULL;
+    head->data = a[0];
+    head->next = NULL;
+    p = head;
+    for (int i = 1; i < size; i++)
+    {
+        temp = new Node;
+        temp->next = NULL;
+        temp->data = a[i];
+        temp->prev = p;
+        p->next = temp;
+        p = temp;
+    }
+    p->next = head;
+    head->prev = p;
+}
+
 void display()
 {
     Node *p = head;
     do
     {
-        p = p->next;
         cout << p->data << " ";
+        p = p->next;
     } while (p != head);
+}
+
+void Delete(int pos)
+{
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    createCircularDoublyLL(arr, size);
+    display();
+    return 0;
 }
