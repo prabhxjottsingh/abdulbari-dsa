a296dd7fa7ab0a6ba374546c17ce752ae4abee2b
Prabhxjott <prabhx.jott@gmail.com>
2022-02-14 17:15:09 +0530

    Trees - By Striver

---
+++ Trees - By Striver/Video 02 - Tree Representation/Representation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+struct Node{
+    int data;
+    struct Node *left, *right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main(){
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+}
